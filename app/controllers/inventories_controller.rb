aa456264eacf63badc5b76d4b33f5263b03e082e
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-07-04 10:43:44 -0600

    Created views for inventory

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,2 +1,52 @@
 class InventoriesController < ApplicationController
+    before_action :set_inventory, only: [:show, :destroy]
+    before_action :require_owner, only: [:show, :destroy]
+  
+    def index
+      @inventories = current_user.inventories
+    end
+  
+    def show
+        @inventory = current_user.inventories.find(params[:id])
+      
+        @inventory_foods = @inventory.inventory_foods
+      
+        @total_food_items = @inventory_foods.sum(:quantity)
+        @total_price = @inventory_foods.joins(:food).sum('foods.price * inventory_foods.quantity')
+      end
+  
+    def new
+      @inventory = current_user.inventories.build
+    end
+  
+    def create
+      @inventory = current_user.inventories.build(inventory_params)
+      if @inventory.save
+        redirect_to @inventory, notice: 'Inventory was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def destroy
+      @inventory.destroy
+      redirect_to inventories_url, notice: 'Inventory was successfully deleted.'
+    end
+  
+    private
+  
+    def set_inventory
+      @inventory = current_user.inventories.find(params[:id])
+    end
+  
+    def require_owner
+      unless @inventory.user == current_user
+        redirect_to inventories_url, alert: 'You are not authorized to perform this action.'
+      end
+    end
+  
+    def inventory_params
+      params.require(:inventory).permit(:name)
+    end
 end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :inventories
+  resources :inventory_foods
   # Defines the root path route ("/")
   # root "articles#index"
 end
