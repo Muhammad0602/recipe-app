cf472cf428a8169deb48085acb739ae1894fb2a0
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-04 22:39:02 +0500

    Add validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,6 @@
 class Food < ApplicationRecord
+    validates :name, presence: true
+
+    has_many :inventory_food
+    has_many :recipe_food
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
+  has_many :inventory, foreign_key: :user_id
+  has_many :recipe, foreign_key: :user_id
 end
