31bcd18b26da2cfd214dd4b1737ed9079202098a
dadadon <davianberoni@yahoo.com>
2023-07-05 09:16:41 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :foods
   resources :users
+  resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
