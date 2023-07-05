780efb966bac6159208a117151ac14b55aed825d
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-05 20:20:24 +0500

    Add migrations to database

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_143049) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_inventories_on_user_id"
+  end
+
+  create_table "inventory_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "inventory_id", null: false
+    t.bigint "food_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_inventory_foods_on_food_id"
+    t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
+  end
+
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
     t.bigint "recipe_id"
@@ -57,6 +75,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_143049) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "inventories", "users"
+  add_foreign_key "inventory_foods", "foods"
+  add_foreign_key "inventory_foods", "inventories"
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
