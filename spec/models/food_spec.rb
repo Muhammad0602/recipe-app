ace69e76b7597bd59e803820abcfb911a278bdd7
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-06 22:00:15 +0500

    Add unit tests for Food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
- 
+  subject { Food.create(name: "Osh") }
+  
+  describe 'validations' do
+    it 'the name should be present' do
+      expect(subject).to be_valid
+    end
+
+    it 'should be not valid when the name is nil' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
