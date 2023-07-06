ebb524b8a84f4ce95a7f2a1df61a5a6b088c3359
dadadon <davianberoni@yahoo.com>
2023-07-06 10:30:48 -0500

    Add function to set current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  before_action :authenticate_user!
+  before_action :set_current_user
+
   protected
 
   def update_allowed_parameters
@@ -10,5 +13,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :surname, :email, :password, :current_password)
     end
+  end
+
+  def set_current_user
+    @current_user = current_user
   end
 end
