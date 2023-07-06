0942c8d916f87bae54f42544de0db3da30ddc86a
dadadon <davianberoni@yahoo.com>
2023-07-06 10:31:00 -0500

    Add recipe routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   resources :foods
   resources :users
-  resources :recipes
+  resources :recipes do
+    resources :recipe_foods
+  end  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :inventories
   resources :inventory_foods
+
+  get 'my-recipes', to: 'recipes#my_recipes'
+  put '/recipes/:id/toggle_public', to: 'recipes#toggle_public', as: 'toggle_public_recipe'
+  get '/recipes/:id/shopping_list', to: 'recipes#shopping_list', as: 'recipe_shopping_list'
+  get 'public-recipes', to: 'recipes#public_recipes'
+  root "recipes#my_recipes"
   # Defines the root path route ("/")
   # root "articles#index"
 end
