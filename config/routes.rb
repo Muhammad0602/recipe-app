ad6edabe8667704e2da4f28f20997ab07bd83355
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-07-06 16:08:16 -0600

    add rout name to fix addressing issue

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get 'my-recipes', to: 'recipes#my_recipes'
+  get 'my-recipes', to: 'recipes#my_recipes' , as: 'my_recipes'
   put '/recipes/:id/toggle_public', to: 'recipes#toggle_public', as: 'toggle_public_recipe'
   get '/recipes/:id/shopping_list', to: 'recipes#shopping_list', as: 'recipe_shopping_list'
   get 'public-recipes', to: 'recipes#public_recipes'
