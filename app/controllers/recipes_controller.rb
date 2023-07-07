74a414b73d23a990f3fcc53c402d076c672591c6
dadadon <davianberoni@yahoo.com>
2023-07-07 10:31:41 -0500

    Fix linter errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -53,29 +53,44 @@ class RecipesController < ApplicationController
     redirect_to @recipe
   end
 
+  # rubocop:disable Metrics/MethodLength
   def shopping_list
     @recipe = Recipe.find(params[:id])
     @inventory = Inventory.find(params[:inventory_id])
+
     recipe_foods = @recipe.recipe_foods
     inventory_foods = @inventory.inventory_foods.index_by(&:food_id)
+
     @missing_items = {}
     @total_price = 0
+
     recipe_foods.each do |recipe_food|
       food = Food.find(recipe_food.food_id)
       quantity_needed = recipe_food.quantity
+
       inventory_food = inventory_foods[food.id]
       next if inventory_food.present? && inventory_food.quantity >= quantity_needed
 
-      missing_quantity = [quantity_needed - (inventory_food&.quantity || 0), 0].max
+      missing_quantity = [quantity_needed - (inventory_food&.quantity || 0), 0].max # Ensures a non-negative quantity
       price = food.price * missing_quantity
-      @missing_items[food.name] = { quantity: missing_quantity, price: }
+
+      @missing_items[food.name] = {
+        quantity: missing_quantity,
+        price:
+      }
       @total_price += price
     end
+
     @amount_of_food_to_buy = @missing_items.length
-    render 'shopping_lists/index',
-           locals: { amount_of_food_to_buy: @amount_of_food_to_buy, recipe: @recipe, total_value_of_food_needed: @total_price,
-                     inventory: @inventory }
+
+    render 'shopping_lists/index', locals: {
+      amount_of_food_to_buy: @amount_of_food_to_buy,
+      recipe: @recipe,
+      total_value_of_food_needed: @total_price,
+      inventory: @inventory
+    }
   end
+  # rubocop:enable Metrics/MethodLength
 
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
